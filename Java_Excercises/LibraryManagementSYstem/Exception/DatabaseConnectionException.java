7a50b147bda7646da735acc02e32fec3ceacccad
lokeswaris-S <lokeswari.s@moolya.com>
2025-11-24 14:49:50 +0530

    Update DatabaseConnectionException.java

--- Java_Excercises/LibraryManagementSYstem/Exception/DatabaseConnectionException.java
+++ Java_Excercises/LibraryManagementSYstem/Exception/DatabaseConnectionException.java
@@ -1,7 +1,11 @@
-package exception;
+package Exception;
 
 public class DatabaseConnectionException extends Exception {
-    public DatabaseConnectionException(String message) {
+	public DatabaseConnectionException(String message) {
         super(message);
+    }
+
+    public DatabaseConnectionException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
