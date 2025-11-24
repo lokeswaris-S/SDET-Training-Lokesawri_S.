d19b9e118872b827db634a381d156fc9cf0f67a1
lokeswaris-S <lokeswari.s@moolya.com>
2025-11-24 14:51:30 +0530

    Update Book.java

--- Java_Excercises/LibraryManagementSYstem/Model/Book.java
+++ Java_Excercises/LibraryManagementSYstem/Model/Book.java
@@ -1,47 +1,53 @@
 package model;
 
 public class Book {
-    private int bookId;
-    private String title;
-    private String author;
-    private boolean isAvailable;
+	private int bookId;
+	private String title;
+	private String author;
+	private boolean isAvailable;
 
-    public Book(int bookId, String title, String author) {
-        this.bookId = bookId;
-        this.title = title;
-        this.author = author;
-        this.isAvailable = true;
-    }
+	public Book(int bookId, String title, String author) {
+		this.bookId = bookId;
+		this.title = title;
+		this.author = author;
+		this.isAvailable = true;
+	}
 
-    public int getBookId() { 
-      return bookId; 
-    }
-    public String getTitle() { 
-      return title; 
-    }
-    public String getAuthor() {
-      return author; 
-    }
-    public boolean isAvailable() { 
-      return isAvailable;
-    }
+	public int getBookId() {
+		return bookId;
+	}
 
-    public void setTitle(String title) { 
-      this.title = title;
-    }
-    public void setAuthor(String author) { 
-      this.author = author; 
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void markAvailable() {
-      this.isAvailable = true;
-    }
-    public void markUnavailable() { 
-      this.isAvailable = false;
-    }
+	public String getAuthor() {
+		return author;
+	}
 
-    public void display() {
-        System.out.println(bookId + " | " + title + " | " + author + 
-            (isAvailable ? " [Available]" : " [Issued]"));
-    }
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	   public void markAvailable() {
+	        this.isAvailable = true;
+	    }
+
+	    public void markUnavailable() {
+	        this.isAvailable = false;
+	    }
+	    
+
+	public void display() {
+		System.out.println(bookId + " | " + title + " | " + author +
+				(isAvailable ? " Available" : " Issued"));
+	}
 }
